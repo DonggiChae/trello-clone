5de055b4ce408209630569de912fece9ddb4767d
Dongi <chaedonggi@gmail.com>
2022-05-01 13:38:45 +0900

    Reordering

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,6 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  textColor: "white",
   bgColor: "#3F8CF2",
   boardColor: "#DADFE9",
   cardColor: "white",
